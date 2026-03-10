e0f627b46f2a8118b4a79b68d72eb97270422d03
CassianKnoth <cassian.knoth@accenture.com>
2026-03-10 10:37:27 +0100

    properly parse json

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,15 @@
 console.log('script!');
 
 const fetchPosts = async () => {
-	const posts = await fetch(
+	const rawPosts = await fetch(
 		'https://cjoeuq75.api.sanity.io/v2023-05-03/data/query/production?query=*%5B_type%20%3D%3D%20%22post%22%5D%7Btitle%2C%20body%2C%20publishedAt%7D',
 	);
 
-	const parsedPosts = JSON.parse(posts);
+	console.log('raw', rawPosts);
+
+	const jsonPosts = await rawPosts.json();
+
+	const parsedPosts = JSON.parse(jsonPosts);
 
 	return parsedPosts;
 };
